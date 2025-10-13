44f6405eedca928fee903e6dfe622f6f07eac188
mohammed abo matter <mohammedabomatter@gmail.com>
2025-10-13 11:37:30 +0300

    refac : extract CustomAppBar widget into seprate file

--- lib/features/home/presentation/views/home_page_view.dart
+++ lib/features/home/presentation/views/home_page_view.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/widgets/home_page_body.dart';
 import 'package:flutter/material.dart';
 
@@ -22,30 +23,4 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({super.key, required this.width, required this.height});
 
-  final double width;
-  final double height;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 38),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Image.asset(AssestData.logo),
-          SizedBox(
-            height: 30,
-            width: 25,
-            child: Image.asset(AssestData.icSearch, fit: BoxFit.fill),
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
-}
