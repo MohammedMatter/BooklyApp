811148cb017d29ad1b09d79b28da0a912e491836
mohammed abo matter <mohammedabomatter@gmail.com>
2025-10-13 13:06:54 +0300

    change viewportFraction

--- lib/features/home/presentation/widgets/home_page_body.dart
+++ lib/features/home/presentation/widgets/home_page_body.dart
@@ -1,4 +1,5 @@
-import 'package:bookly_app/core/utils/styles.dart';
+
+import 'package:bookly_app/features/home/presentation/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/widgets/book_image_container.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ class HomePageBody extends StatefulWidget {
 }
 
 class _HomePageBodyState extends State<HomePageBody> {
-  PageController _pageController = PageController(viewportFraction: 0.6);
+  PageController _pageController = PageController(viewportFraction: 0.5);
   double currentPage = 0;
   @override
   void initState() {
@@ -51,105 +52,10 @@ class _HomePageBodyState extends State<HomePageBody> {
             ),
           ),
           SizedBox(height: widget.height * 0.02),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: List.generate(
-              5,
-              (index) => Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (index == 0)
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 20, top: 50),
-                      child: Text('Best Seller', style: Styles.titleMedium),
-                    ),
-
-                  IntrinsicHeight(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 16),
-                          child: BookImageContainer(width: widget.width),
-                        ),
-                        SizedBox(width: 5),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                width: widget.width * 0.45,
-                                child: Text(
-                                  'Harry Potter and the Goblet of Fire',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'GT font',
-
-                                    fontSize: 20,
-                                  ),
-                                  overflow: TextOverflow.visible,
-                                ),
-                              ),
-
-                              Text(
-                                'J.K. Rowling',
-                                style: TextStyle(color: Color(0xff707070)),
-                              ),
-
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    '19.99 €',
-                                    style: TextStyle(
-                                      fontFamily: 'montserrat',
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-
-                                  Row(
-                                    children: [
-                                      Icon(Icons.star, color: Colors.amber),
-                                      Text(
-                                        '4.8 ',
-                                        style: TextStyle(
-                                          fontFamily: 'montserrat',
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      Text(
-                                        '(2390)',
-                                        style: TextStyle(
-                                          fontFamily: 'montserrat',
-                                          fontWeight: FontWeight.bold,
-                                          color: const Color.fromARGB(
-                                            255,
-                                            70,
-                                            68,
-                                            68,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  SizedBox(width: widget.width * 0.001),
-                                ],
-                              ),
-                              SizedBox(height: widget.height * 0.09),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          BestSelletListViewItem(widget: widget),
         ],
       ),
     );
   }
 }
+
