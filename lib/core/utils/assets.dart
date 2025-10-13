b556721ff6e433caf4c5121d96693a9d287181eb
mohammed abo matter <mohammedabomatter@gmail.com>
2025-10-13 11:27:29 +0300

    add icon search image

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,6 @@
 class AssestData{
 static String logo = 'assets/images/Logo.png' ; 
 static  String book1 = 'assets/images/book1.png' ; 
+static  String icSearch = 'assets/images/ic search.png' ; 
+
 }
