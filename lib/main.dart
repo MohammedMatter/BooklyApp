ceaed349f8173a27fd18645ed2aca0df2f6e8eee
mohammed abo matter <mohammedabomatter@gmail.com>
2025-10-14 10:36:33 +0300

    add config routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/features/home/presentation/views/home_page_view.dart';
-import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,17 +11,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
         appBarTheme: AppBarTheme(backgroundColor: kPrimaryColor),
       ),
-      initialRoute: 'SplashView',
-      routes: {
-        'HomePage': (context) => HomePage(),
-        'SplashView': (context) => SplashView(),
-      },
       debugShowCheckedModeBanner: false,
     );
   }
+
+  
 }
